54ccf115a5ab3218abbcf49ba7659b902eb5dd97
Pablo Pomodoro <p.pomodoro@posteo.com>
2025-03-05 23:30:37 +0100

    feat: start basic todo list functionality

--- src/main.go
+++ src/main.go
@@ -16,39 +16,109 @@ type todoList struct {
 	items []item
 }
 
+func add(todoList *todoList, itemName string) {
+
+	item := item{
+		name:    itemName,
+		dueDate: time.Now(),
+		done:    false,
+	}
+
+	todoList.items = append(todoList.items, item)
+}
+
+func check(itemNumber int) {
+
+}
+
+func remove(itemNumber int) {
+
+}
+
+func clearScreen() {
+	fmt.Print("\033[H\033[2J")
+}
+
 func main() {
 
+	clearScreen()
 	fmt.Println("Welcome to the TODO App!")
+
+	todoList := todoList{}
 
 	var action string
 	for {
-		fmt.Print("\033[H\033[2J")
-		// list
-		fmt.Println("THE LIST THE LIST THE LIST")
+		var emptyInput string
+		fmt.Scanln(&emptyInput)
+		clearScreen()
+
+		if len(todoList.items) == 0 {
+			fmt.Println("No items yet!")
+		} else {
+			for itemIndex, item := range todoList.items {
+				fmt.Printf("%d: %s, %s, %s\n",
+					itemIndex+1,
+					item.name,
+					item.dueDate.Format("2006-01-02"),
+					map[bool]string{true: "✓", false: "✘"}[item.done])
+			}
+		}
+		fmt.Println()
 
 		fmt.Println("Choose actions - add, remove, check, exit")
 		_, err := fmt.Scanln(&action)
 
 		if err != nil {
-			fmt.Println("Invalid action")
+			fmt.Println("Invalid action!")
 			continue
 		}
 
 		switch action {
 		case "add":
-			// add
+			var itemName string
+
+			fmt.Print("Choose item name: ")
+			_, err := fmt.Scanln(&itemName)
+
+			if err != nil {
+				fmt.Println("Invalid item name!")
+				continue
+			}
+
+			add(&todoList, itemName)
 		case "remove":
-			// remove
+			fmt.Println("Which item: ")
+			itemNumber, err := fmt.Scanln()
+
+			if err != nil {
+				fmt.Println("Invalid item!")
+				continue
+			}
+
+			remove(itemNumber)
 		case "check":
-			// check
+			fmt.Println("Which item: ")
+			itemNumber, err := fmt.Scanln()
+
+			if err != nil {
+				fmt.Println("Invalid item!")
+				continue
+			}
+
+			check(itemNumber)
 		case "exit":
 			goto exit
 		default:
-			fmt.Println("Invalid action")
+			fmt.Println("Invalid action!")
 			continue
 		}
 	}
 
 exit:
 	fmt.Println("Goodbye!")
+
+	var emptyInput string
+	fmt.Scanln(&emptyInput)
+
+	clearScreen()
 }
